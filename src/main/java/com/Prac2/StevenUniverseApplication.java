3835af9e96ef43bcdb7d6b4b34b7a214ac943cae
Noelia Segura <156636908+NoeSegura11@users.noreply.github.com>
2025-02-06 00:54:07 -0600

    Primera actualización
    Se asigna el localhost en puerto 80 y se crea el index.

---
+++ src/main/java/com/Prac2/StevenUniverseApplication.java
@@ -0,0 +1,13 @@
+package com.Prac2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StevenUniverseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StevenUniverseApplication.class, args);
+	}
+
+}
